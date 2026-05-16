207f75e647bebbedb42b4f55fcfb92093b947da4
pf-ece <faypatrick07@gmail.com>
2026-05-16 13:42:45 -0400

    Setup for hold mode.

---
+++ source/dl_hold.h
@@ -0,0 +1,23 @@
+/** dl_hold.h | Looper using a delay line.
+ *  Author: Patrick F. (pf-ece)
+ * 
+ *  Copyright 2026 pf-ece
+ *  MIT License
+ */
+
+#pragma once
+#ifndef DSY_DL_HOLD_H
+#define DSY_DL_HOLD_H
+#include "daisysp.h"
+#include "daisy_seed.h"
+namespace daisysp
+{
+class DLHold
+{
+    public:
+        DLHold() {}
+        ~DLHold() {}
+    private:
+};
+};
+#endif
